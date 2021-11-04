0e1cc3103946247868c65e7961eda8f896083972
Pau Larrea <paularrea12@hotmail.com>
2021-11-04 17:45:40 +0100

    dep

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@ module.exports = {
     title: `Bacana Studio`,
     description: `We are an interior design and architecture company focusing on hospitality, workplaces and residential projects around the world. Listening to our clients and understanding their needs, the environment and the communities behind every project is what drives our creativity while strategically developing individual concepts around business models.`,
     author: `Pau Larrea`,
-    siteUrl: `www.bacanastudio.com`,
+    siteUrl: `https://www.bacanastudio.com/`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
